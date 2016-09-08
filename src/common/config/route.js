7ed775d2c3d471de52320bc90ff46bc7f609079f
左盐 <huabinglan@163.com>
2016-09-08 12:07:57 +0800

    修正图片列表的路由规则

--- src/common/config/route.js
+++ src/common/config/route.js
@@ -5,6 +5,6 @@ export default [
     [/([a-z0-9]+)_(\d+)c(\d+)m(\d+)$/, "home/newslist/index?path=:1&nodeid=:2&cp=:3&mp=:4"],//普通文字列表页
     [/([a-z0-9]+)_(\d+)m(\d+)$/, "home/newslist/index?path=:1&nodeid=:2&cp=1&mp=:3"],//普通文字列表页,不带cp
     [/([a-z0-9]+)_(\d+)node$/, "home/newslist/node?path=:1&nodeid=:2"],//节点类型的列表页
-    [/([a-z0-9]+)_(\d+)c(\d+)m(\d+)img$/, "home/newslist/index?path=:1&nodeid=:2&cp=:3&mp=:4"],//图片列表页
-    [/([a-z0-9]+)_(\d+)m(\d+)img$/, "home/newslist/index?path=:1&nodeid=:2&cp=1&mp=:3"],//图片列表页，不带cp
+    [/([a-z0-9]+)_(\d+)c(\d+)m(\d+)img$/, "home/newslist/img?path=:1&nodeid=:2&cp=:3&mp=:4"],//图片列表页
+    [/([a-z0-9]+)_(\d+)m(\d+)img$/, "home/newslist/img?path=:1&nodeid=:2&cp=1&mp=:3"],//图片列表页，不带cp
 ]
