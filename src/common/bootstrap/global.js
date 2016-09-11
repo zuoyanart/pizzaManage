6a4fbe9b4ab46845151876ce10722512eb65fca9
左盐 <huabinglan@163.com>
2016-09-11 22:48:32 +0800

    init

--- src/common/bootstrap/global.js
+++ src/common/bootstrap/global.js
@@ -271,3 +271,29 @@ global.getInt = (value, def = 0) => {
 global.removeHtml = (str = "") => {
     return str.replace(/<[^>]+>/g,"").replace(/&nbsp;/g, "");
 }
+
+/**
+ * 验证是否为智能手机
+ * @ param {string} data :this.userAgent;
+ * @ return {bool}
+ */
+/** global checkMobile */
+global.checkMobile = function(agent) {
+    let flag = false;
+    agent = agent.toLowerCase();
+    let keywords = ["android", "iphone", "ipod", "ipad", "windows phone", "mqqbrowser"];
+
+    //排除 Windows 桌面系统
+    if (!(agent.indexOf("windows nt") > -1) || (agent.indexOf("windows nt") > -1 && agent.indexOf("compatible; msie 9.0;") > -1)) {
+        //排除苹果桌面系统
+        if (!(agent.indexOf("windows nt") > -1) && !agent.indexOf("macintosh") > -1) {
+            for (let item of keywords) {
+                if (agent.indexOf(item) > -1) {
+                    flag = true;
+                    break;
+                }
+            }
+        }
+    }
+    return flag;
+}
