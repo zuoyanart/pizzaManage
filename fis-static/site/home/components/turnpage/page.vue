4819613412026a1f8d5e355c019a20797b271898
左盐 <huabinglan@163.com>
2017-02-07 18:15:55 +0800

    添加翻页

---
+++ fis-static/site/home/components/turnpage/page.vue
@@ -0,0 +1,57 @@
+<style lang="less">
+
+@import "./page.less";
+
+</style>
+
+<template lang="html">
+  <div class="pz-turnpage">
+    <!-- 首页、上一页、下一页，数字，末页 -->
+     <template v-if="pageType == 'hunde'">
+
+    </template>
+    <!-- 首页、上一页、下一页 -->
+    <template v-if="pageType == 'hud'">
+
+    </template>
+    <!-- 首页、上一页、下一页 -->
+    <template  v-if="pageType == 'hud'">
+
+    </template>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    pageType: {
+       type: String,
+       default: "hunde"
+    },
+    changePage: {//切换翻页
+      type: Function,
+      default: function(){}
+    },
+    sum: {//总条数
+      type: Number,
+      default:0
+    },
+    total: {//总页数
+      type: Number,
+      default:0
+    },
+    mp: {
+      type: Number,
+      default:20
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+}
+
+</script>
